7a323de96495646faa80ffcf6d16e9b64b77079f
Dileepa Ameresekere <dileepa.ameresekere@gmail.com>
2018-09-23 21:48:11 +1000

    adding an empty module suite with one module to test travis ci

---
+++ module1/src/au/com/doopz/module1/ExampleUtilities.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.com.doopz.module1;
+
+/**
+ *
+ * @author doopz
+ */
+public class ExampleUtilities {
+    
+    public static long getCurrentMilliseconds(){
+        return System.currentTimeMillis();
+    }
+    
+}
