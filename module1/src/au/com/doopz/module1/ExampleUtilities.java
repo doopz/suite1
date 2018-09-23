f015ca3d574176da9c18181f3c4764bab6b69f94
Dileepa Ameresekere <dileepa.ameresekere@gmail.com>
2018-09-23 21:50:29 +1000

    test travis ci

--- module1/src/au/com/doopz/module1/ExampleUtilities.java
+++ module1/src/au/com/doopz/module1/ExampleUtilities.java
@@ -10,9 +10,13 @@ package au.com.doopz.module1;
  * @author doopz
  */
 public class ExampleUtilities {
-    
-    public static long getCurrentMilliseconds(){
+
+    public static long getCurrentMilliseconds() {
         return System.currentTimeMillis();
     }
-    
+
+    public static long getCurrentSeconds() {
+        return System.currentTimeMillis() / 1000;
+    }
+
 }
